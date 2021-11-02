859768269ed2d8fa1e7add47bc400a9a9b298ff7
bpr <boguslaw.proszkowiec@gmail.com>
2021-11-02 19:01:32 +0100

    Implement corona info panel

--- corona-extension@bproszkowiec/extension.js
+++ corona-extension@bproszkowiec/extension.js
@@ -1,11 +1,206 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
+const GObject = imports.gi.GObject;
+const Gio = imports.gi.Gio;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const settings = Me.imports.lib.getSettings();
-let updateInfo = Me.imports.lib.CoronaInfo.getInstance();
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
 
-let panelButton, panelButtonText, timeout, refreshPeriod = 60.0;
+let updateInfo = Me.imports.lib.CoronaInfo.getInstance();
+let timeout,
+  refreshPeriod = 60.0;
+var NOT_APPLICABLE = "n/a";
+var SKULL = "\u2620";
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+let coronaMenuButton = GObject.registerClass(
+  class CoronaMenuButton extends PanelMenu.Button {
+    _init() {
+      super._init(0);
+
+      this.panelButton = new St.Bin({
+        style_class: "panel-button",
+      });
+      this.panelButtonText = new St.Label({
+        style_class: "coronaInfoPanelText",
+        text: "Starting...",
+      });
+      this.panelButton.set_child(this.panelButtonText);
+      this.add_child(this.panelButton);
+
+      this._currentCountryInfo = new St.Bin({});
+      let _itemCurrent = new PopupMenu.PopupBaseMenuItem({
+        reactive: false,
+      });
+      _itemCurrent.actor.add_actor(this._currentCountryInfo);
+
+      this.menu.addMenuItem(_itemCurrent);
+
+      this.buildCurrentCountryData(null);
+    }
+
+    update(coronaData) {
+      this.rebuildCurrentCountryData(coronaData);
+    }
+
+    destroyCurrentCountryData() {
+      if (this._currentCountryInfo.get_child() !== null)
+        this._currentCountryInfo.get_child().destroy();
+    }
+
+    rebuildCurrentCountryData(coronaData) {
+      this.destroyCurrentCountryData();
+      this.buildCurrentCountryData(coronaData);
+    }
+
+    buildCurrentCountryData(coronaData) {
+      log(String(Me.dir.get_path()));
+
+      this._newCasesIcon = new St.Icon({
+        icon_size: 15,
+        gicon: Gio.icon_new_for_string(
+          Me.dir.get_path() + "/icons/man-shape.svg"
+        ),
+        style_class: "new-cases-icon-style",
+      });
+
+      this._newDeathsIcon = new St.Icon({
+        icon_size: 15,
+        gicon: Gio.icon_new_for_string(
+          Me.dir.get_path() + "/icons/sick-face.svg"
+        ),
+        style_class: "new-deaths-icon-style",
+      });
+
+      this._currentCountrySummary = new St.Label({
+        text: coronaData == null ? "---" : numberWithCommas(coronaData.country),
+        style_class: "current-country-summary",
+      });
+      this._countryLabel = new St.Label({
+        text: _("Country:"),
+      });
+
+      let bb = new St.BoxLayout({
+        vertical: true,
+        style_class: "system-menu-action corona-current-summarybox",
+      });
+      bb.add_actor(this._countryLabel);
+      bb.add_actor(this._currentCountrySummary);
+
+      this._newCases = new St.Label({
+        text:
+          coronaData == null ? "---" : numberWithCommas(coronaData.newCases),
+      });
+      this._newDeaths = new St.Label({
+        text:
+          coronaData == null ? "---" : numberWithCommas(coronaData.newDeaths),
+      });
+
+      let ab = new St.BoxLayout({
+        style_class: "corona-current-infobox",
+      });
+
+      ab.add_actor(this._newCasesIcon);
+      ab.add_actor(this._newCases);
+      ab.add_actor(this._newDeathsIcon);
+      ab.add_actor(this._newDeaths);
+      bb.add_actor(ab);
+
+      // Other labels
+      this._totalCases = new St.Label({
+        text:
+          coronaData == null ? "---" : numberWithCommas(coronaData.totalCases),
+      });
+      this._totalDeaths = new St.Label({
+        text:
+          coronaData == null ? "---" : numberWithCommas(coronaData.totalDeaths),
+      });
+      this._recovered = new St.Label({
+        text:
+          coronaData == null ? "---" : numberWithCommas(coronaData.recovered),
+      });
+      this._activeSick = new St.Label({
+        text: coronaData == null ? "---" : numberWithCommas(coronaData.active),
+      });
+      this._criticalSick = new St.Label({
+        text:
+          coronaData == null ? "---" : numberWithCommas(coronaData.critical),
+      });
+      this._casesPerMilion = new St.Label({
+        text:
+          coronaData == null
+            ? "---"
+            : numberWithCommas(coronaData.casesPerMilion),
+      });
+      let rb = new St.BoxLayout({
+        style_class: "corona-current-databox",
+      });
+      let rb_captions = new St.BoxLayout({
+        vertical: true,
+        style_class:
+          "popup-menu-item popup-status-menu-item corona-current-databox-captions",
+      });
+      let rb_values = new St.BoxLayout({
+        vertical: true,
+        style_class: "system-menu-action corona-current-databox-values",
+      });
+      rb.add_actor(rb_captions);
+      rb.add_actor(rb_values);
+
+      rb_captions.add_actor(
+        new St.Label({
+          text: _("Total cases:"),
+        })
+      );
+      rb_values.add_actor(this._totalCases);
+      rb_captions.add_actor(
+        new St.Label({
+          text: _("Total deaths:"),
+        })
+      );
+      rb_values.add_actor(this._totalDeaths);
+      rb_captions.add_actor(
+        new St.Label({
+          text: _("Recovered:"),
+        })
+      );
+      rb_values.add_actor(this._recovered);
+      rb_captions.add_actor(
+        new St.Label({
+          text: _("Active:"),
+        })
+      );
+      rb_values.add_actor(this._activeSick);
+      rb_captions.add_actor(
+        new St.Label({
+          text: _("Critical:"),
+        })
+      );
+      rb_values.add_actor(this._criticalSick);
+      rb_captions.add_actor(
+        new St.Label({
+          text: _("Cases / 1M:"),
+        })
+      );
+      rb_values.add_actor(this._casesPerMilion);
+
+      let xb = new St.BoxLayout();
+      xb.add_actor(bb);
+      xb.add_actor(rb);
+
+      let box = new St.BoxLayout({
+        style_class: "corona-current-iconbox",
+      });
+      box.add_actor(xb);
+      this._currentCountryInfo.set_child(box);
+    }
+  }
+);
 
 function timerHandler() {
   updateButtonText();
@@ -23,32 +218,33 @@ function updateIntervalChangedHandler() {
 }
 
 function init() {
-  panelButton = new St.Bin({
-    style_class: "panel-button",
-  });
-  panelButtonText = new St.Label({
-    style_class: "coronaInfoPanelText",
-    text: "Starting...",
-  });
-  panelButton.set_child(panelButtonText);
   refreshPeriod = settings.get_int("update-interval") * 60;
 
-  settings.connect('changed::' + "country", countryChangedHandler);
-  settings.connect('changed::' + "update-interval", updateIntervalChangedHandler);
+  settings.connect("changed::" + "country", countryChangedHandler);
+  settings.connect(
+    "changed::" + "update-interval",
+    updateIntervalChangedHandler
+  );
 }
 
 function updateButtonText() {
   let data = updateInfo.updateCoronaInfo();
-  panelButtonText.set_text(data);
+
+  let panelText = `${data.newCases}  ${SKULL} ${
+    data.newDeaths == NOT_APPLICABLE ? data.newDeaths : data.newDeaths
+  }`;
+  coronaMenuButton.panelButtonText.set_text(panelText);
+  coronaMenuButton.update(data);
 }
 
 function enable() {
-  Main.panel._rightBox.insert_child_at_index(panelButton, 1);
   timeout = Mainloop.timeout_add_seconds(refreshPeriod, timerHandler);
+
+  coronaMenuButton = new coronaMenuButton();
+  Main.panel.addToStatusArea("CoronaMenuButton", coronaMenuButton, 1);
   updateButtonText();
 }
 
 function disable() {
   Mainloop.source_remove(timeout);
-  Main.panel._rightBox.remove_child(panelButton);
 }
--- corona-extension@bproszkowiec/lib.js
+++ corona-extension@bproszkowiec/lib.js
@@ -3,7 +3,6 @@ var GLib = imports.gi.GLib;
 var Me = imports.misc.extensionUtils.getCurrentExtension();
 
 var URL = "curl https://corona-stats.online/";
-var SKULL = "\u2620";
 var NOT_APPLICABLE = "n/a";
 
 class CoronaInfo {
@@ -22,38 +21,40 @@ class CoronaInfo {
     let [ok, out, err, exit] = GLib.spawn_command_line_sync(URL + country);
     var data = collectData(String(out), country);
     return data;
-  };
+  }
 }
 
 removeColorFormatting = (text) => text.replace(/\u001b\[.*?m/g, "");
-  
-removeNotNumber =  (text) => text.replace(/[^0-9]+/g, "");
 
-function collectData (httmlData, selectedCountry) {
+removeNotNumber = (text) => text.replace(/[^0-9]+/g, "");
+
+function collectData(httmlData, selectedCountry) {
   var regex = `${selectedCountry}.*║`;
   var countryRow = httmlData
     .match(regex)
     .toString()
     .replace(/║|,|\s/gi, "");
-    var countryDataArray = String(countryRow).split(/\│/);
+  var countryDataArray = String(countryRow).split(/\│/);
 
-  var country = removeColorFormatting(countryDataArray[0]);
-  var totalCases = removeColorFormatting(countryDataArray[1]);
-  var newCases = removeColorFormatting(countryDataArray[2]);
-  if (newCases == "") newCases = NOT_APPLICABLE;
-  var totalDeaths = removeColorFormatting(countryDataArray[3]);
-  var newDeaths = removeColorFormatting(countryDataArray[4]);
-  if (newDeaths == "") newDeaths = NOT_APPLICABLE;
-  var recovered = removeColorFormatting(countryDataArray[5]);
-  var active = removeColorFormatting(countryDataArray[6]);
-  var critical = removeColorFormatting(countryDataArray[7]);
-  var casesPerMilion = removeColorFormatting(countryDataArray[8]);
+  const data = {
+    country: removeColorFormatting(countryDataArray[0].replace(/\(.*\)/gi, "")),
+    totalCases: removeColorFormatting(countryDataArray[1]),
+    newCases: removeColorFormatting(countryDataArray[2]),
+    totalDeaths: removeColorFormatting(countryDataArray[3]),
+    newDeaths: removeColorFormatting(countryDataArray[4]),
+    recovered: removeColorFormatting(countryDataArray[5]),
+    active: removeColorFormatting(countryDataArray[6]),
+    critical: removeColorFormatting(countryDataArray[7]),
+    casesPerMilion: removeColorFormatting(countryDataArray[8]),
+  };
 
-  var textToShow = `${newCases}  ${SKULL} ${
-    newDeaths == NOT_APPLICABLE ? newDeaths : removeNotNumber(newDeaths)
-  }`;
-  return textToShow;
-};
+  data.newCases =
+    data.newCases == "" ? NOT_APPLICABLE : removeNotNumber(data.newCases);
+  data.newDeaths =
+    data.newDeaths == "" ? NOT_APPLICABLE : removeNotNumber(data.newDeaths);
+
+  return data;
+}
 
 function getSettings() {
   var GioSSS = Gio.SettingsSchemaSource;
